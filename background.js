a342ff43ffc4d1fe1d1ff8d774dbbf1947d48730
Ameer Ahmed <ameer@clara.doctor>
2023-03-31 07:12:05 -0400

    refactor, default context menu items with separators, create or remove custom context menu items

--- background.js
+++ background.js
@@ -1,51 +1,10 @@
+const extensionName = 'WriteGenius';
+const extensionSysName = 'Core';
+
 chrome.runtime.onInstalled.addListener(() => {
-  // updated context menu
   chrome.contextMenus.create({
     id: 'chrome-chatgpt',
-    title: 'WriteGenius',
-    contexts: ['selection'],
-  });
-
-  chrome.contextMenus.create({
-    parentId: 'chrome-chatgpt',
-    id: 'rephrase:',
-    title: 'Reprhase',
-    contexts: ['selection'],
-  });
-
-  chrome.contextMenus.create({
-    parentId: 'chrome-chatgpt',
-    id: 'rephrase with humor:',
-    title: 'Reprhase (funny)',
-    contexts: ['selection'],
-  });
-
-  chrome.contextMenus.create({
-    parentId: 'chrome-chatgpt',
-    id: 'rephrase professionaly:',
-    title: 'Reprhase (professional)',
-    contexts: ['selection'],
-  });
-
-  chrome.contextMenus.create({
-    parentId: 'chrome-chatgpt',
-    id: 'summarize in one line:',
-    title: 'Summarize (one liner)',
-    contexts: ['selection'],
-  });
-
-  chrome.contextMenus.create({
-    parentId: 'chrome-chatgpt',
-    id: 'summarize in 5 points:',
-    title: 'Summarize (5 points)',
-    contexts: ['selection'],
-  });
-
-  // Add separator
-  chrome.contextMenus.create({
-    parentId: 'chrome-chatgpt',
-    id: 'separator',
-    type: 'separator',
+    title: extensionName,
     contexts: ['selection'],
   });
 
@@ -59,37 +18,125 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     parentId: 'chrome-chatgpt',
     id: 'continue',
-    title: 'Continue Prompt',
+    title: 'Continue prompt',
     contexts: ['selection'],
   });
+
+  // Add separator
+  chrome.contextMenus.create({
+    parentId: 'chrome-chatgpt',
+    id: 'separator2',
+    type: 'separator',
+    contexts: ['selection'],
+  });
+
+  chrome.contextMenus.create({
+    parentId: 'chrome-chatgpt',
+    id: 'Rephrase:',
+    title: 'Reprhase',
+    contexts: ['selection'],
+  });
+
+  chrome.contextMenus.create({
+    parentId: 'chrome-chatgpt',
+    id: 'Rephrase with wit:',
+    title: 'Reprhase (funny)',
+    contexts: ['selection'],
+  });
+
+  chrome.contextMenus.create({
+    parentId: 'chrome-chatgpt',
+    id: 'Rephrase professionaly:',
+    title: 'Reprhase (professional)',
+    contexts: ['selection'],
+  });
+
+  // Add separator
+  chrome.contextMenus.create({
+    parentId: 'chrome-chatgpt',
+    id: 'separator3',
+    type: 'separator',
+    contexts: ['selection'],
+  });
+
+  chrome.contextMenus.create({
+    parentId: 'chrome-chatgpt',
+    id: 'Summarize:',
+    title: 'Summarize',
+    contexts: ['selection'],
+  });
+  chrome.contextMenus.create({
+    parentId: 'chrome-chatgpt',
+    id: 'Summarize in one line:',
+    title: 'Summarize (one liner)',
+    contexts: ['selection'],
+  });
+
+  chrome.contextMenus.create({
+    parentId: 'chrome-chatgpt',
+    id: 'Summarize in 5 points:',
+    title: 'Summarize (5 points)',
+    contexts: ['selection'],
+  });
+
   chrome.storage.sync.get('customContextMenuItems', ({ customContextMenuItems }) => {
-    console.log('customContextMenuItems', customContextMenuItems);
-    if (customContextMenuItems) {
+    if (customContextMenuItems && customContextMenuItems.length > 0) {
+      updateRootContextMenu(extensionSysName);
+      isCustomMenuVisibile(true);
       createCustomContextMenuItems(customContextMenuItems);
     }
   });
 });
 
+function updateRootContextMenu(title) {
+  chrome.contextMenus.update('chrome-chatgpt', {
+    title: title,
+  });
+}
+
+function isCustomMenuVisibile(isTrue) {
+  if (isTrue) {
+    updateRootContextMenu(extensionSysName);
+    chrome.contextMenus.create({
+      id: 'separator0',
+      type: 'separator',
+      contexts: ['selection'],
+    });
+    chrome.contextMenus.create({
+      id: 'chrome-chatgpt-custom',
+      title: 'Custom',
+      contexts: ['selection'],
+    });
+  } else {
+    updateRootContextMenu(extensionName);
+    chrome.contextMenus.remove('separator0');
+    chrome.contextMenus.remove('chrome-chatgpt-custom');
+  }
+}
+
 chrome.contextMenus.onClicked.addListener((info, tabs) => {
+  if (info.menuItemId === 'chrome-chatgpt') {
+    return;
+  }
   info.type = 'contextMenu';
   chrome.tabs.sendMessage(tabs.id, info);
 });
 
 function createCustomContextMenuItems(items) {
-  items.forEach(({ id, parentId, title, position }) => {
+  items.forEach(({ id, title }) => {
     chrome.contextMenus.create({
       id: id,
-      parentId: parentId,
+      parentId: 'chrome-chatgpt-custom',
       title: title,
       contexts: ['selection'],
     });
   });
 }
 
-function createCustomContextMenuItem({ id, parentId, title, position }) {
+function createCustomContextMenuItem({ id, title }) {
   chrome.contextMenus.create({
     id: id,
-    parentId: parentId,
+    parentId: 'chrome-chatgpt-custom',
     title: title,
     contexts: ['selection'],
   });
@@ -111,12 +158,19 @@ chrome.storage.onChanged.addListener((changes, areaName) => {
     const oldValues = changes.customContextMenuItems.oldValue;
     if (newValues.length > oldValues.length) {
       // add item
+      if (newValues.length === 1) {
+        isCustomMenuVisibile(true);
+      }
       item = newValues[newValues.length - 1];
       createCustomContextMenuItem(item);
     } else {
       // remove item
       item = getFirstDiff(newValues, oldValues);
       chrome.contextMenus.remove(item.id);
+    }
+
+    if (newValues.length === 0) {
+      isCustomMenuVisibile(false);
     }
   }
 });
